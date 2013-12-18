277073544eab1da3f66e79842b81f14620cc5baa
youfuli <youfuli.fly@gmail.com>
2013-12-18 18:13:15 +0800

    modify the xml operation classes

---
+++ src/main/java/com/cattles/util/XMLOperationCluster.java
@@ -0,0 +1,11 @@
+package com.cattles.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: youfuli
+ * Date: 12/18/13
+ * Time: 3:36 PM
+ * read and modify the VirtualClusters.xml
+ */
+public class XMLOperationCluster {
+}
---
+++ src/main/java/com/cattles/util/XMLOperationPlatform.java
@@ -0,0 +1,11 @@
+package com.cattles.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: youfuli
+ * Date: 12/18/13
+ * Time: 3:44 PM
+ * read the platform_conf.xml
+ */
+public class XMLOperationPlatform {
+}
