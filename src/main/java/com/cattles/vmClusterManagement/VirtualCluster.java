fa9d6d9cc5162d01b41759b26c3b947cfea3a953
youfuli <youfuli.fly@gmail.com>
2013-12-22 06:31:58 -0800

    modify the xml operation classes

--- src/main/java/com/cattles/vmClusterManagement/VirtualCluster.java
+++ src/main/java/com/cattles/vmClusterManagement/VirtualCluster.java
@@ -3,9 +3,9 @@ package com.cattles.vmClusterManagement;
 import java.util.ArrayList;
 
 public class VirtualCluster {
-	String clusterState;//if the cluster is in "standby" state
-	int clusterSize;//cluster size, the number of worker
-	String clusterServerID;//the server ID in this cluster
+	String clusterState;  //if the cluster is in "standby" state
+	int clusterSize;   //cluster size, the number of worker
+	String clusterServerID;    //the server ID in this cluster
     ArrayList<String> nodesIDList;
 	public void addVM() {
 		
