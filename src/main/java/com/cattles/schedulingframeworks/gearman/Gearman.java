bca1ca6e8f4c23bc63e4a12a7860b257a13ea940
youfuli <youfuli.fly@gmail.com>
2013-12-26 23:25:42 +0800

    import the ssh module to enable the execution of shell command remotely

---
+++ src/main/java/com/cattles/schedulingframeworks/gearman/Gearman.java
@@ -0,0 +1,11 @@
+package com.cattles.schedulingframeworks.gearman;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: youfuli
+ * Date: 12/26/13
+ * Time: 9:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Gearman {
+}
