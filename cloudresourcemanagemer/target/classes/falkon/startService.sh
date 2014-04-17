d884c8568ccf5f375bc13593a8e4f3cc725e0a84
youfuli <youfuli.fly@gmail.com>
2014-04-17 11:57:42 +0800

    refactor the module schedulingmanagementservice

--- cloudresourcemanagemer/target/classes/falkon/startService.sh
+++
@@ -1,2 +0,0 @@
-source /usr/local/falkon.r174/falkon.env
-falkon-service-stdout.sh 50001 ${FALKON_CONFIG}/Falkon.config
--- cloudresourcemanagemer/target/classes/falkon/startWorker.sh
+++
@@ -1,3 +0,0 @@
-serviceIP=$1
-source /usr/local/falkon.r174/falkon.env 
-falkon-worker-stdout.sh $serviceIP 50001
