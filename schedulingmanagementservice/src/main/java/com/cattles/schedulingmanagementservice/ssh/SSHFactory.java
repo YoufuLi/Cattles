e9ad45aad55481a664a2c1058aa84f2a3e52267d
youfuli <youfuli.fly@gmail.com>
2014-07-15 21:44:56 +0800

    revise the README file

---
+++ schedulingmanagementservice/src/main/java/com/cattles/schedulingmanagementservice/ssh/SSHFactory.java
@@ -0,0 +1,10 @@
+package com.cattles.schedulingmanagementservice.ssh;
+
+/**
+ * rerurn the CommandExecutable instance
+ *
+ * @author Tom
+ */
+public interface SSHFactory {
+    public CommandExecutable getCmdExec();
+}
